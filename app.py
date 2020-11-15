2cf9b7189a878332a84aebf2b756aa0125aaecf8
Wertzhog <ljupko.jurcevic@gmail.com>
2020-11-15 13:31:45 +0000

    add delete product functionality

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def home():
 
     return render_template("index.html")
 
+
 #Register functionality
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -48,6 +49,7 @@ def register():
         return redirect(url_for('profile', username=session["user"]))
     return render_template("register.html")
 
+
 #Login functionality
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -67,6 +69,7 @@ def login():
             return redirect(url_for("login"))
     return render_template("login.html")
 
+
 #Logout functionality
 @app.route("/logout")
 def logout():
@@ -74,17 +77,18 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
 #Profile functionality
 @app.route("/profile/<username>", methods = ["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one({"username": session["user"]})["username"]
     curr_user = mongo.db.users.find_one({"username": session["user"]})
     products_sold = mongo.db.products_sold.find()
-    #Add var to target products user for displaying and editing...aand adding
     if session["user"]:
         return render_template("profile.html", username=username, curr_user=curr_user, products_sold=products_sold)
 
     return redirect(url_for("login"))
+
 
 #Add product functionality
 @app.route("/add_product", methods = ["GET", "POST"])
@@ -104,14 +108,30 @@ def add_product():
     return render_template("add_product.html", product_cat=product_cat)
 
 
+#Edit product functionality
 @app.route("/edit_product/<product_id>", methods=["GET","POST"])
 def edit_product(product_id):
+    if request.method == "POST":
+        product_update = {
+            "category": request.form.get("category"),
+            "name": request.form.get("name"),
+            "price": request.form.get("price"),
+            "quantity": request.form.get("quantity"),
+            "sold_by": session["user"]
+        }
+        mongo.db.products_sold.update({"_id": ObjectId(product_id)}, product_update)
+        flash("Product successfully updated!")
+        return redirect (url_for("profile", username=session["user"]))
     product_sold = mongo.db.products_sold.find_one({"_id": ObjectId(product_id)})
     product_cat = mongo.db.product_categories.find().sort("category", 1)
     return render_template("edit_product.html", product_sold=product_sold,product_cat=product_cat)
 
 
-
+@app.route("/delete_product/<product_id>")
+def delete_product(product_id):
+    mongo.db.products_sold.remove({"_id": ObjectId(product_id)})
+    flash("Product successfully deleted!")
+    return redirect (url_for("profile", username=session["user"]))
 
 
 if __name__ == "__main__":
